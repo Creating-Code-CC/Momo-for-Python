684b5bd9856fd80b08fd5fbe79b0b71350e54e4e
Malik Smith <smithmalik2468@gmail.com>
2023-01-26 21:27:03 +0000

    main added

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
-import kivy
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
-from kivy.uix.gridlayout import GridLayout
+import speech_recognition as sr
+import pyttsx3
+class Speak:
+    def Command(self):
+        self.recognizer=sr.Recognizer()
+        self.microphone=sr.Microphone()
+        # check that recognizer and microphone arguments are appropriate type
+        if not isinstance(self.recognizer, sr.Recognizer):
+            raise TypeError("`recognizer` must be `Recognizer` instance")
 
-class MainScreen(GridLayout):   # Home Page
-    def __init__(self, **kwargs):
-        super(MainScreen, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text='Momo'))
+        if not isinstance(self.microphone, sr.Microphone):
+            raise TypeError("`microphone` must be `Microphone` instance")
 
-class MoMo(App):
-    def build(self):
-        return MainScreen()
-
-if __name__ == "__main__":  # run() triggers the class MoMo(App)
-        MoMo().run()
+        # adjust the recognizer sensitivity to ambient noise and record audio
+        # from the microphone
+        with self.microphone as source:
+            self.recognizer.adjust_for_ambient_noise(source)
+            self.audio = self.recognizer.listen(source)
+    
+        try:
+            cmd=self.recognizer.recognize_google(self.audio)
+        except sr.RequestError:
+            # API was unreachable or unresponsive
+            cmd = "API unavailable"
+        except sr.UnknownValueError:
+            # speech was unintelligible
+            cmd="Unable to recognize speech"
+            
+        return cmd
